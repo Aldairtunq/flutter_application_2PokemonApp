978022395333a6265a9c17cb551d7a80bd9be8ce
Aldairtunq <201912945b@utea.edu.pe>
2022-11-28 12:30:28 -0500

    commit VIDEO16 ALDAIR

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+//uri permite identificar y localizar
+//url localizador de reurso del servidor
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-pokedex/master/pokedex.json");
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("data"),
+          onPressed: () {
+            getDataPokemon();
+          },
+        ),
+      ),
+    );
   }
 }

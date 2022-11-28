4c2aa489bdeae2d2d0f9a983f2a287c1e87225fc
Aldairtunq <201912945b@utea.edu.pe>
2022-11-28 11:48:44 -0500

    commit /video 15 ALDAIR

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_2/pages/home_page.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "POKEDEX MYAPPALDAIRTUNQUIPA",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}

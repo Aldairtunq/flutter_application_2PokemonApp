181feddf93e8741f1b7601ab27e97f812433a2b4
Aldairtunq <201912945b@utea.edu.pe>
2022-11-28 23:02:04 -0500

    commit video 17  ALDAIR

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,12 +18,101 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("data"),
-          onPressed: () {
-            getDataPokemon();
-          },
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Text(
+                  "POKEDEX",
+                  style: TextStyle(
+                    fontSize: 22.9,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12.0,
+                  crossAxisSpacing: 12.0,
+                  childAspectRatio: 1.35,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(217, 114, 186, 162),
+                          borderRadius: BorderRadius.circular(18.0)),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: -20,
+                            right: -20,
+                            child: Image.asset(
+                              'assets/images/imagenpoke.png',
+                              height: 120.0,
+                              color: Colors.white.withOpacity(0.27),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 12.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Bulbasaur",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  margin:
+                                      const EdgeInsets.symmetric(vertical: 6.0),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 14, vertical: 4.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.2),
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.04),
+                                          offset: const Offset(4, 4),
+                                          blurRadius: 12.0,
+                                        )
+                                      ]),
+                                  child: Text(
+                                    "Grass",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Image.network(
+                              "https://www.serebii.net/pokemongo/pokemon/001.png",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
